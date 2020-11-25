21ba7ca9f5c3c9697d391169870b95d0e91e29c0
Elek Ákos <elekakos2703@gmail.com>
2020-11-25 13:18:23 +0100

    Add service, agencies and proxy

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { GTFSAPIService, Agencies } from './gtfs-api.service';
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  private agencies: Agencies[] = [];
+
+  constructor(private GTFS: GTFSAPIService) {}
+
+  async ngOnInit() {
+    this.agencies = await this.GTFS.getAgencies();
+    console.log(this.agencies);
+  }
 }
--- frontend/src/app/gtfs-api.service.ts
+++ frontend/src/app/gtfs-api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
+const GTFS_API_END = '/api/v1';
+
+export interface Agencies {
+  agencyId: string;
+  agencyName: string;
+  agencyUrl: string;
+  agencyTimezone: string;
+  agencyPhone: string;
+  agencyLang: string;
+}
 @Injectable({
   providedIn: 'root'
 })
-export class GtfsApiService {
+export class GTFSAPIService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getAgencies(): Promise<Agencies[]> {
+    return this.http.get<Agencies[]>(`${GTFS_API_END}/agencies`).toPromise();
+  }
 }
